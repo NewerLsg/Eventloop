0f5b10902c98df934a6ae198995d9e8c4f9467d6
NewerLsg <247520226@qq.com>
2016-07-04 23:56:34 +0800

    commmit

--- src/nw_evtloop.h
+++ src/nw_evtloop.h
@@ -3,7 +3,14 @@
 #include<stdio.h>
 #include"nw_list.h"
 
+#define EVT_READ 	0x001
+#define EVT_WRITE 	0x010
+
+#define set_read_mask(events) 	events |=0x001
+#define set_write_mask(events) 	events |=0x010
+
 typedef struct evtobj{
+	int fd;
 	int events;	
 	void (*whandler)(struct evtobj *ev, char *buff, size_t size);
 	void (*rhandler)(struct evtobj *ev, char *buff, size_t size);
